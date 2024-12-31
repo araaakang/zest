2112b6084d26747c8ad6ef5ec627b8c329da68bf
Jennifer <jennifer881030@gmail.com>
2024-12-31 10:33:16 +0800

    Add a bottom tab navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerTitle: 'Sticker Smash', headerLeft: () => <></> }}
-      />
-      <Stack.Screen name="about" options={{ headerTitle: 'About' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" options={{ headerShown: false }} />
     </Stack>
   );
