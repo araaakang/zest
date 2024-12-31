dfca456c101da655da9be8280bc17dc32da7a0d6
Jennifer <jennifer881030@gmail.com>
2024-12-31 11:47:38 +0800

    Divide components into files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
 
-const PlaceholderImage = require('../../assets/images/background-image.png');
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
     </View>
   );
@@ -18,11 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#25292e',
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
   imageContainer: {
     flex: 1,
