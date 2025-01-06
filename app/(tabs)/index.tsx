1d635e35c04bce6bb7cfbdfd8ada6f0fd27f5a8c
Jennifer <jennifer881030@gmail.com>
2025-01-06 09:41:18 +0800

    Build basic layout structure

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from 'react-native';
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Zest!</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#25292e',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
---
+++ app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'expo-router';
+import { Text, View, StyleSheet } from 'react-native';
+export default function NotFoundScreen() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Oops! Not Found</Text>
+        <Link href="/" style={styles.button}>
+          Go back to Home screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#25292e',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
